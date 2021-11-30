1dd1df55652123ef9266128d97aa1d6684b0db14
Yusuke_Takahashi <meteora2.71828@gmail.com>
2021-11-30 16:49:55 +0900

    doskoi

---
+++ code/2_4.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  final title = 'Flutter Sample';
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      home: new MyHomePage(
+        title: this.title,
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({required this.title}) : super();
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late String _message;
+
+  @override
+  void initState() {
+    super.initState();
+    _message = 'Hello';
+  }
+
+  void _setMessage() {
+    setState(() {
+      _message = 'Tapped';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Text(
+        _message,
+        style: TextStyle(fontSize: 32.0),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _setMessage,
+        tooltip: 'set message',
+        child: Icon(Icons.ad_units_rounded),
+      ),
+    );
+  }
+}
